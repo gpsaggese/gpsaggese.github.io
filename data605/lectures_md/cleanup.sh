648452096c39bb3e318eaeeae5fbe661dc1e26d3
GP Saggese <saggese@gmail.com>
2025-09-20 12:01:32 -0400

    Update

--- data605/lectures_md/cleanup.sh
+++ data605/lectures_md/cleanup.sh
@@ -1,5 +1,7 @@
 # Remove 3/57
 perl -ne 'print unless /^[[:space:]]*\d+\/\d+[[:space:]]*$/;' $*
+
+# Remove the tag
 perl -i -ne 'print unless /UMD DATA605/;' $*
 
 # Merge
@@ -11,3 +13,6 @@ perl -i -ne 'print unless /UMD DATA605/;' $*
 # - **Challenges**
 perl -i -0777 -pe 's/\*\*\s*\n\s*\*\*/\*\*/g' $*
 perl -i -0777 -pe 's/\*\*([^\n]+?)\s*\n\s*\*\*/**$1**/g' $*
+
+# Clean up 
+perl -i -0777 -pe 's/\n{3,}/\n\n/g' $*
