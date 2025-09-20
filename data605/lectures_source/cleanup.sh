a3355f2800fef91fd8dc3d00782d2b6a9d4d5076
GP Saggese <saggese@gmail.com>
2025-09-20 12:15:08 -0400

    Update

--- data605/lectures_source/cleanup.sh
+++ data605/lectures_source/cleanup.sh
@@ -1,8 +1,11 @@
+#!/bin/bash -xe
+FILE=$1
+
 # Remove 3/57
-perl -ne 'print unless /^[[:space:]]*\d+\/\d+[[:space:]]*$/;' $*
+perl -ne 'print unless /^[[:space:]]*\d+\/\d+[[:space:]]*$/;' $FILE
 
 # Remove the tag
-perl -i -ne 'print unless /UMD DATA605/;' $*
+perl -i -ne 'print unless /UMD DATA605/;' $FILE
 
 # Merge
 # ```
@@ -11,8 +14,11 @@ perl -i -ne 'print unless /UMD DATA605/;' $*
 # ```
 # into
 # - **Challenges**
-perl -i -0777 -pe 's/\*\*\s*\n\s*\*\*/\*\*/g' $*
-perl -i -0777 -pe 's/\*\*([^\n]+?)\s*\n\s*\*\*/**$1**/g' $*
+perl -i -0777 -pe 's/\*\*\s*\n\s*\*\*/\*\*/g' $FILE
+perl -i -0777 -pe 's/\*\*([^\n]+?)\s*\n\s*\*\*/**$1**/g' $FILE
 
 # Clean up 
-perl -i -0777 -pe 's/\n{3,}/\n\n/g' $*
+perl -i -0777 -pe 's/\n{3,}/\n\n/g' $FILE
+
+# Reflow
+lint_txt.py -i $FILE --use_dockerized_prettier
