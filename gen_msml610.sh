a4396d6060181f95a39b656fe3916dacf0c91e5f
GP Saggese <saggese@gmail.com>
2025-09-21 10:18:08 -0400

    Update

--- gen_msml610.sh
+++ gen_msml610.sh
@@ -1,3 +1,4 @@
-#
+#!/bin/bash
 LESSON=$1
-SRC_NAME=$(cd msml610/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input msml610/lectures_source/$SRC_NAME --output msml610/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error --filter_by_slides 1:20
+DIR=msml610
+SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error
