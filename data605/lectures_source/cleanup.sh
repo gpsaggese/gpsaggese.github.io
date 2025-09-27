09c2c999a1f99d2a4e559376692648136ff9eef7
GP Saggese <saggese@gmail.com>
2025-09-26 20:17:39 -0400

    Update

--- data605/lectures_source/cleanup.sh
+++ data605/lectures_source/cleanup.sh
@@ -7,6 +7,9 @@ perl -ne 'print unless /^[[:space:]]*\d+\/\d+[[:space:]]*$/;' $FILE
 # Remove the tag
 perl -i -ne 'print unless /UMD DATA605/;' $FILE
 
+# Remove duplicated lines
+perl -i -ne 'print if !defined($prev) || $_ ne $prev; $prev = $_' $FILE
+
 # Merge
 # ```
 # - **Challenges
@@ -21,6 +24,11 @@ perl -i -0777 -pe 's/\*\*([^\n]+?)\s*\n\s*\*\*/**$1**/g' $FILE
 perl -i -0777 -pe 's/\n{3,}/\n\n/g' $FILE
 
 perl -i -pe 's/→/$\to$/g' $FILE
+
+# Convert '**git pull**: short hand from **git fetch origin**'
+# to
+# `git pull`: short hand from `git fetch origin`
+perl -i -pe 's/\*\*([^*]+)\*\*/`\1`/g' $FILE
 
 # Reflow
 lint_txt.py -i $FILE --use_dockerized_prettier
