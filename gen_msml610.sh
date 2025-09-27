878e4171b1e07891c2396e76cb36a3f21916fd28
GP Saggese <saggese@gmail.com>
2025-09-27 17:53:43 -0400

    Update

--- gen_msml610.sh
+++ gen_msml610.sh
@@ -2,4 +2,4 @@
 LESSON=$1
 DIR=msml610
 OPTS=${@:2}
-SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error $OPTS
+SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error --skip_action cleanup_after $OPTS
