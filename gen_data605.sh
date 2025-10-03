e7444dd5c8fe5af3f5755908a82cd922cd7b02eb
GP Saggese <saggese@gmail.com>
2025-10-02 20:40:09 -0400

    Update

--- gen_data605.sh
+++ gen_data605.sh
@@ -1,10 +1,10 @@
-!/bin/bash
+#!/bin/bash -xe
 LESSON=$1
 DIR=data605
 
 shopt -s nullglob   # empty pattern expands to nothing instead of itself
 
-files=($DIR/lectures_source/Lesson0${LESSON}*)
+files=($DIR/lectures_source/Lesson${LESSON}*)
 if (( ${#files[@]} == 0 )); then
     echo "No files found"
     exit 1
@@ -12,4 +12,4 @@ else
     echo "Found files: ${files[*]}"
 fi
 
-SRC_NAME=$(cd $DIR/lectures_source; ls Lesson0${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error
+SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error
