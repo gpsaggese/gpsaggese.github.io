fda7f4e5c6819141db91d2323954f7ec20b7672a
GP Saggese <saggese@gmail.com>
2025-10-02 20:40:09 -0400

    Update

--- gen_msml610.sh
+++ gen_msml610.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -xe
 LESSON=$1
 DIR=msml610
-OPTS=${@:2}
-SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error --skip_action cleanup_after $OPTS
+
+shopt -s nullglob   # empty pattern expands to nothing instead of itself
+
+files=($DIR/lectures_source/Lesson${LESSON}*)
+if (( ${#files[@]} == 0 )); then
+    echo "No files found"
+    exit 1
+else
+    echo "Found files: ${files[*]}"
+fi
+
+SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*); DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/'); notes_to_pdf.py --input $DIR/lectures_source/$SRC_NAME --output $DIR/lectures/$DST_NAME --type slides --toc_type navigation --debug_on_error
