edd80922bd911c2712b6cd70a9a62a76a5b4b27e
GP Saggese <saggese@gmail.com>
2025-10-17 17:24:35 -0400

    Update

--- gen_data605.sh
+++ gen_data605.sh
@@ -16,8 +16,10 @@ OPTS=${@:2}
 
 SRC_NAME=$(cd $DIR/lectures_source; ls Lesson${LESSON}*)
 DST_NAME=$(echo $SRC_NAME | sed 's/\.txt$/.pdf/')
+OPTS_DEBUG="--skip_action cleanup_before --skip_action cleanup_after"
 notes_to_pdf.py \
     --input $DIR/lectures_source/$SRC_NAME \
     --output $DIR/lectures/$DST_NAME \
     --type slides --toc_type navigation --debug_on_error \
+    $OPTS_DEBUG \
     $OPTS
