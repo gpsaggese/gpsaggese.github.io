c9f0514b16f646b452112702dc7dc6db6b376d25
GP Saggese <saggese@gmail.com>
2025-12-07 20:08:28 -0500

    Update

--- count_pages.sh
+++ count_pages.sh
@@ -2,4 +2,4 @@
 DIR=data605
 
 cd $DIR/lectures/
-find Lesson0*.pdf -type f -name "*.pdf" -print0 | while IFS= read -r -d '' file; do     pages=$(mdls -name kMDItemNumberOfPages "$file" | awk -F'= ' '{print $2}');     echo -e "${file}\t${pages}"; done
+find Lesson*.pdf -type f -name "*.pdf" -print0 | while IFS= read -r -d '' file; do     pages=$(mdls -name kMDItemNumberOfPages "$file" | awk -F'= ' '{print $2}');     echo -e "${file}\t${pages}"; done
