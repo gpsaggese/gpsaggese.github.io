0a1473178c00ddef7a0835a8f5d85a308ba53a6b
GP Saggese <saggese@gmail.com>
2025-12-28 14:44:52 -0500

    Update

---
+++ class_project/create_PR.sh
@@ -0,0 +1,5 @@
+class_project/create_PR.py \
+      --input_file class_project/fall2025_msml610_branches.txt \
+      --source_dir /Users/saggese/src/umd_classes2 \
+      --dst_dir /Users/saggese/src/umd_classes3 \
+      --output_file output.txt
