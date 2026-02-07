587b8ab3b63124814c957e8751bbc104bdc3dd2d
utkrisht12 <nathutkrisht@gmail.com>
2026-02-07 15:09:34 -0500

    Added docker scripts

---
+++ tutorials/GluonTS_COVID19_Prediction/docker_name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Docker image naming configuration.
+# This file is sourced by docker_*.sh scripts.
+
+REPO_NAME=gpsaggese
+IMAGE_NAME=gluonts_covid19_prediction
+FULL_IMAGE_NAME=$REPO_NAME/$IMAGE_NAME
