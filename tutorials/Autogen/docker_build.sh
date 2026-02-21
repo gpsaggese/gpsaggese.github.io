949711744c8507d0f57c08ee3065b2a16f4aa568
PranavShashidhara <44715972+PranavShashidhara@users.noreply.github.com>
2026-02-21 12:48:34 -0500

    UmdTask317_Fall2025_Create_Blog_Tutorial_on_Autogen (#329)
    * Initial commit for Autogen tutorial branch
    
    * Update Utils.py and example.ipynb
    
    * Update Docker Setup and add autogen.API.ipynb
    
    * Update autoge.API.ipynb
    
    * Update autogen.API.ipynb
    
    * Update git autogen.API.ipynb
    
    * Add Final changes to autogen.example.ipynb and autogen.API.ipynb files
    
    Pre-commit checks:
    All checks passed ✅

---
+++ tutorials/Autogen/docker_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit immediately if any command exits with a non-zero status.
+set -e
+
+# Print each command to stdout before executing it.
+set -x
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+source $GIT_ROOT/class_project/docker_common/utils.sh
+
+# Source Docker image naming configuration.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $SCRIPT_DIR/docker_name.sh
+
+# Build container.
+#export DOCKER_BUILDKIT=1
+export DOCKER_BUILDKIT=0
+
+docker build . -t $FULL_IMAGE_NAME
