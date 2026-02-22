c8630d425527f03988091022dece5c142ff3b163
GP Saggese <saggese@gmail.com>
2026-02-21 19:54:13 -0500

    Update

--- tutorials/CausalML_Diabetes_Study/docker_jupyter.sh
+++ tutorials/CausalML_Diabetes_Study/docker_jupyter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # """
-# Execute Jupyter Notebook in a Docker container.
+# Execute Jupyter Lab in a Docker container.
 #
-# This script launches a Docker container running Jupyter Notebook with
+# This script launches a Docker container running Jupyter Lab with
 # configurable port, directory mounting, and vim bindings. It passes
 # command-line options to the run_jupyter.sh script inside the container.
 #
@@ -28,7 +28,7 @@ OLD_CMD_OPTS=$@
 while getopts p:d:uv flag
 do
     case "${flag}" in
-        p) JUPYTER_HOST_PORT=${OPTARG};;  # Port for Jupyter Notebook
+        p) JUPYTER_HOST_PORT=${OPTARG};;  # Port for Jupyter Lab
         u) JUPYTER_USE_VIM=1;;            # Enable vim bindings
         d) TARGET_DIR=${OPTARG};;         # Directory to mount as /data
         v) VERBOSE=1;;                    # Enable verbose output
@@ -50,9 +50,7 @@ source $DOCKER_NAME
 print_docker_vars
 
 # Configure Docker run options with port forwarding and optional volume mount.
-# We use the same port inside and outside the container, so that the localhost
-# printed inside the container is the correct one.
-DOCKER_RUN_OPTS="-p $JUPYTER_HOST_PORT:$JUPYTER_HOST_PORT"
+DOCKER_RUN_OPTS="-p $JUPYTER_HOST_PORT:8888"
 if [[ $TARGET_DIR != "" ]]; then
     DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS -v $TARGET_DIR:/data"
 fi;
@@ -62,7 +60,7 @@ CMD="/curr_dir/run_jupyter.sh $OLD_CMD_OPTS"
 run "docker image ls $FULL_IMAGE_NAME"
 (docker manifest inspect $FULL_IMAGE_NAME | grep arch) || true
 
-# Run the Docker container with Jupyter Notebook.
+# Run the Docker container with Jupyter Lab.
 CONTAINER_NAME=$IMAGE_NAME
 run "docker run \
     --rm -ti \
